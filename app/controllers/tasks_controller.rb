e3cb8aa5424a85dbecff203b8d7f2d6c3550dc50
thibault belair <belair.thibault@gmail.com>
2019-08-13 12:53:46 +0200

    almost done, path issue

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,34 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+  end
+
 end
